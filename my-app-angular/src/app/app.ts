2b4971d6b131df3cd2f233013afbbfa9a645fd85
linharious <linh.t.tn14@gmail.com>
2026-03-05 23:19:00 -0500

    directives in app.component

--- my-app-angular/src/app/app.ts
+++ my-app-angular/src/app/app.ts
@@ -1,13 +1,39 @@
+import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('my-app-angular');
-  protected readonly name = signal('linharious');
+  title = 'my-app-angular';
+  name = 'linharious';
+  imageUrl = 'https://static.boredpanda.com/blog/wp-content/uploads/2024/10/Cw3eFuEqgb8-png__880.jpg';
+
+  items = ['item1', 'item2', 'item3', 'item4', 'item5', 'item6', 'item7', 'item8', 'item9', 'item10'];
+  isActive = true;
+  textColor = 'red';
+  fontSize = 50;
+
+  isLoggedIn = true;
+
+  clickButton() {
+    console.log('button clicked');
+    this.name = 'badass cat';
+    this.imageUrl = "https://i.chzbgr.com/full/10433114112/h4D24531B/mylifern";
+    this.isActive = true;
+    this.textColor = 'green';
+    this.fontSize--;
+  }
+
+  logout() {
+    this.isLoggedIn = false;
+  }
+
+  login() {
+    this.isLoggedIn = true;
+  }
 }
